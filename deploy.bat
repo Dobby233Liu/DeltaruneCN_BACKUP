2abf8e6c55f404714c1d9ebcd0ab3bbfe423339e
DeltaruneCN <1494339027@qq.com>
2019-04-06 17:23:36 +0800

    push

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,5 @@
 @echo off
 cd "%~dp0"
 git add * && git commit -m "push" && git push https://gitee.com/deltarunecn/deltarunecn.git master -f && git push https://github.com/DeltaruneCN/DeltaruneCN.github.io.git master -f
+echo done
 pause>nul
