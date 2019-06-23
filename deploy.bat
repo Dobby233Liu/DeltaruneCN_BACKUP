bae48a4d2f4488d457c332aafaabd03378c29913
Liu-Wen-Yuan <15816141883@163.com>
2019-06-23 16:03:43 +0800

    jj

--- deploy.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd "%~dp0"
-git add * && git commit -m "push" && git push https://gitee.com/deltarunecn/deltarunecn.git master -f && git push https://github.com/DeltaruneCN/DeltaruneCN.github.io.git master -f
-echo 完成上传.
-pause>nul
